6635ed00ef9e7017466261afdc17a47644102f7d
swzhangslg <swzhangslg@qq.com>
2021-01-12 21:55:29 +0800

    test2

--- fe/bench/workload.py
+++ fe/bench/workload.py
@@ -10,9 +10,10 @@ from fe import conf
 
 logging.basicConfig(
       level= logging.INFO,       #设置日志的显示级别为最低一级
-      filename="logger.log",      #设置日志的显示文件名
-      filemode='w',
-      format='%(asctime)s %(filename)s [%(lineno)d] %(message)s',  #设置一个输出模板格式
+    
+#     filename="logger.log",      #设置日志的显示文件名
+#     filemode='w',
+#     format='%(asctime)s %(filename)s [%(lineno)d] %(message)s',  #设置一个输出模板格式
  )
 
 class NewOrder:
