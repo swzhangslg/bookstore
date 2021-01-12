b676640e9913d8e5786206bf83795712dcd4f0f8
swzhangslg <swzhangslg@qq.com>
2021-01-12 21:46:01 +0800

    test

--- fe/bench/workload.py
+++ fe/bench/workload.py
@@ -8,6 +8,12 @@ from fe.access.new_buyer import register_new_buyer
 from fe.access.buyer import Buyer
 from fe import conf
 
+logging.basicConfig(
+      level= logging.INFO,       #设置日志的显示级别为最低一级
+      filename="logger.log",      #设置日志的显示文件名
+      filemode='w',
+      format='%(asctime)s %(filename)s [%(lineno)d] %(message)s',  #设置一个输出模板格式
+ )
 
 class NewOrder:
     def __init__(self, buyer: Buyer, store_id, book_id_and_count):
